a92cf61d99a8e70a4d10d909b7983929dc042d72
Bruno Tognoli <bruno.tognoli@aluno.senai.br>
2025-10-24 09:25:25 -0300

    Alteração 3
    Conclusão dos exercícios 10a à 13.

--- 13_exclusao.php
+++ 13_exclusao.php
@@ -1,4 +1,74 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 <!-- Passar id via URL -->
 <!-- http://localhost/php-basicos/13_exclusao.php?id=5-->
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php
+//Conecta com o banco de dados
+$servername = "localhost";
+$username = "root";
+$password = "Senai@118";
+$dbname = "exercicios";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verifica a conexão
+if ($conn->connect_error) {
+    die("Falha na conexão: " . $conn->connect_error);
+}
+
+//Verifica se o ID foi passado via URL para exclusão
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+
+    // Deleta o registro do cliente com o ID especificado
+    $sql = "DELETE FROM usuarios WHERE id = '$id'";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Cliente excluido com sucesso!";
+    } else {
+        echo "Erro ao excluir cliente: " . $conn->error . "</p>";
+    }
+}
+
+// Fecha a conexão
+$conn->close();
+?>
